5d7b0cd34f9b81c903d4ef73bdbab074564e7bca
RomanKosmyna <roma.kosmyna@gmail.com>
2023-02-02 21:02:15 +0200

    Lesson 6 Homework Part2 | useReducer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import {CommentsPage, HomePage, PostsPage} from "./pages";
-import {MainLayout} from "./layouts";
-import {useRoutes} from "react-router-dom";
+import {useReducer} from "react";
+import {logDOM} from "@testing-library/react";
 
 const App = () => {
-    let routes = [
-        {element: <MainLayout/>, path: '/',
-        children: [
-            {element: <HomePage/>, index: true},
-            {element: <PostsPage/>, path: '/posts'},
-            {element: <CommentsPage/>, path: '/comments'}
-        ]}
-    ];
 
-    let element = useRoutes(routes);
+    function reducer() {
 
+    }
+
+    function initValue() {
+        return {cats: [], dogs: []}
+    }
+
+    const [state, dispatch] = useReducer(reducer, null, initValue);
+
+    console.log(state)
   return (
       <div>
-          {element}
+          <div className={'formBlock'}>
+              <form>
+                  Add Cat:
+                  <input type="text" placeholder={'cat'}/>
+                  <button onClick={() => console.log()}>Save</button>
+
+                  Add Dog:
+                  <input type="text" placeholder={'dog'}/>
+                  <button>Save</button>
+              </form>
+          </div>
+          <hr/>
+          <div className={'infoBlock'}>
+              <div className={'cat'}></div>
+              <div className={'dog'}></div>
+          </div>
       </div>
   );
 };
